25a5540334e1a641b7727f3a71eafe0bd9479973
yakov maharat <54322333+yakov-maharat@users.noreply.github.com>
2020-07-05 21:25:13 +0300

    fixed the logout

--- My_Project-master/server/client/src/VolunteersLogin.js
+++ My_Project-master/server/client/src/VolunteersLogin.js
@@ -36,7 +36,7 @@ class VolunteersLogin extends Component {
             this.loginvolunteers();
             console.log('enter was clicked')
         }
-    }
+    } 
 
     render() {
         const disabled = !this.state.email || !this.state.password;
